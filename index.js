f2325e4f3f5fc7c5ae794ad8fb5f1b95a4da74c6
Zach SL <mzschandy@gmail.com>
2021-03-08 07:13:11 -0500

    static directory fix

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ app.use(cors())
 app.use(bodyParser.json())
 const directory = path.join(__dirname, './images');
 app.use("/images", express.static(directory))
-
+app.use('/api', postRouter )
+app.use(express.static('client/build'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('*', (req, res) => {
-    //res.redirect('/blog')
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 
-app.use('/api', postRouter )
+
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
